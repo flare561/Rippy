92a73ce5c7aa6a85151560109f4f6aac26d682c7
Matt LaPaglia <mlapaglia@gmail.com>
2017-07-26 09:42:42 -0400

    cleanup

--- Rippy/MainWindow.xaml.cs
+++ Rippy/MainWindow.xaml.cs
@@ -121,27 +121,6 @@ namespace Rippy
             }
         }
 
-        public static String MakeRelativePath(String fromPath, String toPath)
-        {
-            if (String.IsNullOrEmpty(fromPath)) throw new ArgumentNullException("fromPath");
-            if (String.IsNullOrEmpty(toPath)) throw new ArgumentNullException("toPath");
-
-            Uri fromUri = new Uri(fromPath);
-            Uri toUri = new Uri(toPath);
-
-            if (fromUri.Scheme != toUri.Scheme) { return toPath; } // path can't be made relative.
-
-            Uri relativeUri = fromUri.MakeRelativeUri(toUri);
-            String relativePath = Uri.UnescapeDataString(relativeUri.ToString());
-
-            if (toUri.Scheme.Equals("file", StringComparison.InvariantCultureIgnoreCase))
-            {
-                relativePath = relativePath.Replace(Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar);
-            }
-
-            return relativePath;
-        }
-
         /// <summary>
         /// 
         /// </summary>
